2bccda31016977cf92a22cec6ddd888c59dc2424
Martin Schinz <m.schinz@gmail.com>
2013-07-24 12:06:32 +0100

    adding download nuget on build and repository interface

--- RenderTheMatrix.Web/Controllers/MatrixController.cs
+++ RenderTheMatrix.Web/Controllers/MatrixController.cs
@@ -3,14 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LibGit2Sharp;
+
 
 namespace RenderTheMatrix.Web.Controllers
 {
     public class MatrixController : Controller
     {
+        private GitCommits _gitCommits;
+
+        public MatrixController()
+        {
+            this._gitCommits = new GitCommits();
+        }
+
         public ActionResult Render()
         {
             return View();
+        }
+
+        [HttpGet]
+        public JsonResult GetColumns(int columnCount)
+        {
+            var columnData =
+                Enumerable.Range(0, columnCount)
+                .Select(_ => _gitCommits.GetNextCommit());
+
+            return Json(new { columns = columnData }, JsonRequestBehavior.AllowGet);
+        }
+    }
+
+    public class GitCommits
+    {
+        private Repository _gitRepository;
+
+        private const string RepositoryPath = @"C:\repo";
+
+        public GitCommits()
+        {
+            this._gitRepository = new Repository(RepositoryPath);
+        }
+
+        public IEnumerable<int> GetNextCommit()
+        {
+            throw new NotImplementedException();
         }
     }
 }
